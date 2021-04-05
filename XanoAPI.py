521aebf1d2890eca977beab61b03ba014b360156
willianmascimiano <wmsilva191@gmail.com>
2021-04-05 01:30:01 -0300

    Update XanoAPI.py

--- XanoAPI.py
+++ XanoAPI.py
@@ -2,8 +2,8 @@ import requests
 
 
 
-# Acessar https://xano.io/1zgzwnvj e criar uma instância. 
-base_url= "https://x8ki-letl-twmt.n7.xano.io/api:vFrDc6RD/speedtest"
+# Acessar https://xano.io/ e criar uma instância. 
+base_url= "https://my_url/speedtest"
 
 def speedTestRecordSave(data_atual,hora_atual,velocidade):
     requests.post(base_url, data={'data':  data_atual, 
