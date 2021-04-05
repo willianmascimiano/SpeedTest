7827d484411abe91f5e99bda23480a2baa804438
Willian Mascimiano <willian@MacBook-Air-de-Willian.local>
2021-04-05 00:28:48 -0300

    Acrescentado o backend Xeno

--- TestSpeed.py
+++ TestSpeed.py
@@ -3,6 +3,13 @@ from datetime import datetime
 import pandas as pd
 from threading import Timer
 
+import requests
+
+
+# Acessar https://xano.io/1zgzwnvj e criar uma instância. 
+base_url= "https://url/speedtest"
+
+
 # função para gravar dados da velocidade da internet
 def internet():
     df = pd.read_excel('dados.xlsx', sheet_name='base')
@@ -12,6 +19,10 @@ def internet():
     velocidade = s.download(threads=None)*(10**-6)
     df.loc[len(df)] = [data_atual, hora_atual, round(velocidade)]
     df.to_excel('dados.xlsx', sheet_name='base', index=False)
+
+    requests.post(base_url, data={'data':  data_atual, 'hora':hora_atual, 'velocidade' : velocidade})
+
+
     Timer(30,internet).start()
 
 
